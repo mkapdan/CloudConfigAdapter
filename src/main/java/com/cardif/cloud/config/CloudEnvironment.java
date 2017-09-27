f756b59157e5f9cb8eaf284c676eceedb3286424
mkapdan <mustafakapdan@gmail.com>
2017-09-28 01:29:25 +0300

    config loading

--- src/main/java/com/cardif/cloud/config/CloudEnvironment.java
+++ src/main/java/com/cardif/cloud/config/CloudEnvironment.java
@@ -14,9 +14,12 @@ public class CloudEnvironment extends StandardServletEnvironment {
     @Override
     protected void customizePropertySources(MutablePropertySources propertySources) {
         super.customizePropertySources(propertySources);
+
+        String oldSystemPropertyAppName=System.getProperty(CloudConfigConstants.CONFIG_SERVER_APP_NAME);
         try {
 
             if (Boolean.TRUE.toString().equalsIgnoreCase(this.getProperty(CloudConfigConstants.CONFIG_SERVER_ENABLE, CloudConfigConstants.CONFIG_SERVER_DEFAULT_ENABLE))) {
+                System.setProperty(CloudConfigConstants.CONFIG_SERVER_APP_NAME,ApplicationNameReader.readSpringApplicationName());
                 PropertySource<?> propertySource = initConfigServicePropertySourceLocator(this);
                 if (null != propertySource) {
                     propertySources.addLast(propertySource);
@@ -25,6 +28,7 @@ public class CloudEnvironment extends StandardServletEnvironment {
 
         } catch (Exception ex) {
 
+            System.setProperty(CloudConfigConstants.CONFIG_SERVER_APP_NAME,oldSystemPropertyAppName);
             if (Boolean.TRUE.toString().equalsIgnoreCase(this.getProperty(CloudConfigConstants.CONFIG_SERVER_FAIL_FAST, CloudConfigConstants.CONFIG_SERVER_DEFAULT_FAIL_FAST))) {
                 throw new RuntimeException("Could not connect to the config server!!!");
 
@@ -40,15 +44,15 @@ public class CloudEnvironment extends StandardServletEnvironment {
         ConfigClientProperties configClientProperties = new ConfigClientProperties(environment);
 
         configClientProperties.setUri(environment.getProperty(CloudConfigConstants.CONFIG_SERVER_ADDRESS, CloudConfigConstants.CONFIG_SERVER_DEFAULT_ADDRESS));
-        configClientProperties.setName(ApplicationNameReader.readSpringApplicationName());
+        configClientProperties.setName(environment.getProperty(CloudConfigConstants.CONFIG_SERVER_APP_NAME, CloudConfigConstants.CONFIG_SERVER_DEFAULT_APP_NAME));
         configClientProperties.setLabel(environment.getProperty(CloudConfigConstants.CONFIG_SERVER_BRANCH, CloudConfigConstants.CONFIG_SERVER_DEFAULT_BRANCH));
         configClientProperties.setUsername(environment.getProperty(CloudConfigConstants.CONFIG_SERVER_USERNAME, CloudConfigConstants.CONFIG_SERVER_DEFAULT_USERNAME));
         configClientProperties.setPassword(environment.getProperty(CloudConfigConstants.CONFIG_SERVER_PASSWORD, CloudConfigConstants.CONFIG_SERVER_DEFAULT_PASSWORD));
         configClientProperties.setProfile(environment.getProperty(CloudConfigConstants.CONFIG_SERVER_PROFILE, CloudConfigConstants.CONFIG_SERVER_DEFAULT_PROFILE));
         configClientProperties.setFailFast(!CloudConfigConstants.CONFIG_SERVER_DEFAULT_FAIL_FAST.equalsIgnoreCase(environment.getProperty(CloudConfigConstants.CONFIG_SERVER_FAIL_FAST, CloudConfigConstants.CONFIG_SERVER_DEFAULT_FAIL_FAST)));
         configClientProperties.setFailFast(!CloudConfigConstants.CONFIG_SERVER_DEFAULT_ENABLE.equalsIgnoreCase(environment.getProperty(CloudConfigConstants.CONFIG_SERVER_ENABLE, CloudConfigConstants.CONFIG_SERVER_DEFAULT_ENABLE)));
-
         ConfigServicePropertySourceLocator configServicePropertySourceLocator = new ConfigServicePropertySourceLocator(configClientProperties);
+
         return configServicePropertySourceLocator.locate(environment);
     }
 
