99d2beaad53650f37dfdac95e13a0077d7796935
mkapdan <mustafakapdan@gmail.com>
2017-09-27 21:45:28 +0300

    config loading

--- src/main/java/com/cardif/cloud/config/CloudConfigConstants.java
+++ src/main/java/com/cardif/cloud/config/CloudConfigConstants.java
@@ -7,6 +7,7 @@ public class CloudConfigConstants {
     public static final String CONFIG_SERVER_PASSWORD = "cardif.cloud.config.password";
     public static final String CONFIG_SERVER_FAIL_FAST = "cardif.cloud.config.failFast";
     public static final String CONFIG_SERVER_BRANCH = "cardif.cloud.config.label";
+    public static final String CONFIG_SERVER_ENABLE = "cardif.cloud.config.enable";
     public static final String CONFIG_SERVER_APP_NAME = "spring.application.name";
     public static final String CONFIG_SERVER_PROFILE = "spring.profiles.active";
 
@@ -17,6 +18,8 @@ public class CloudConfigConstants {
     public static final String CONFIG_SERVER_DEFAULT_USERNAME = null;
     public static final String CONFIG_SERVER_DEFAULT_PASSWORD = null;
     public static final String CONFIG_SERVER_DEFAULT_FAIL_FAST ="false";
+    public static final String CONFIG_SERVER_DEFAULT_ENABLE="false";
+
     public static final String CONFIG_SERVER_DEFAULT_PROFILE = "default";
 
 
--- src/main/java/com/cardif/cloud/config/CloudEnvironment.java
+++ src/main/java/com/cardif/cloud/config/CloudEnvironment.java
@@ -14,10 +14,23 @@ public class CloudEnvironment extends StandardServletEnvironment {
     protected void customizePropertySources(MutablePropertySources propertySources) {
         super.customizePropertySources(propertySources);
         try {
-            propertySources.addLast(initConfigServicePropertySourceLocator(this));
+
+            if (Boolean.TRUE.toString().equalsIgnoreCase(this.getProperty(CloudConfigConstants.CONFIG_SERVER_ENABLE, CloudConfigConstants.CONFIG_SERVER_DEFAULT_ENABLE))) {
+                PropertySource<?> propertySource = initConfigServicePropertySourceLocator(this);
+                if (null != propertySource) {
+                    propertySources.addLast(propertySource);
+                }
+            }
 
         } catch (Exception ex) {
+
+            if (Boolean.TRUE.toString().equalsIgnoreCase(this.getProperty(CloudConfigConstants.CONFIG_SERVER_FAIL_FAST, CloudConfigConstants.CONFIG_SERVER_DEFAULT_FAIL_FAST))) {
+                throw new RuntimeException("Could not connect to the config server!!!");
+
+            }
             logger.warn("failed to initialize cloud config environment", ex);
+
+
         }
     }
 
@@ -25,16 +38,17 @@ public class CloudEnvironment extends StandardServletEnvironment {
 
         ConfigClientProperties configClientProperties = new ConfigClientProperties(environment);
 
-        configClientProperties.setUri(environment.getProperty(CloudConfigConstants.CONFIG_SERVER_ADDRESS,CloudConfigConstants.CONFIG_SERVER_DEFAULT_ADDRESS));
-        configClientProperties.setName(environment.getProperty(CloudConfigConstants.CONFIG_SERVER_APP_NAME,CloudConfigConstants.CONFIG_SERVER_DEFAULT_APP_NAME));
-        configClientProperties.setLabel(environment.getProperty(CloudConfigConstants.CONFIG_SERVER_BRANCH,CloudConfigConstants.CONFIG_SERVER_DEFAULT_BRANCH));
-        configClientProperties.setUsername(environment.getProperty(CloudConfigConstants.CONFIG_SERVER_USERNAME,CloudConfigConstants.CONFIG_SERVER_DEFAULT_USERNAME));
-        configClientProperties.setPassword(environment.getProperty(CloudConfigConstants.CONFIG_SERVER_PASSWORD,CloudConfigConstants.CONFIG_SERVER_DEFAULT_PASSWORD));
-        configClientProperties.setProfile(environment.getProperty(CloudConfigConstants.CONFIG_SERVER_PROFILE,CloudConfigConstants.CONFIG_SERVER_DEFAULT_PROFILE));
-        configClientProperties.setFailFast(CloudConfigConstants.CONFIG_SERVER_DEFAULT_FAIL_FAST.equalsIgnoreCase(environment.getProperty(CloudConfigConstants.CONFIG_SERVER_FAIL_FAST,CloudConfigConstants.CONFIG_SERVER_DEFAULT_FAIL_FAST)));
+        configClientProperties.setUri(environment.getProperty(CloudConfigConstants.CONFIG_SERVER_ADDRESS, CloudConfigConstants.CONFIG_SERVER_DEFAULT_ADDRESS));
+        configClientProperties.setName(environment.getProperty(CloudConfigConstants.CONFIG_SERVER_APP_NAME, CloudConfigConstants.CONFIG_SERVER_DEFAULT_APP_NAME));
+        configClientProperties.setLabel(environment.getProperty(CloudConfigConstants.CONFIG_SERVER_BRANCH, CloudConfigConstants.CONFIG_SERVER_DEFAULT_BRANCH));
+        configClientProperties.setUsername(environment.getProperty(CloudConfigConstants.CONFIG_SERVER_USERNAME, CloudConfigConstants.CONFIG_SERVER_DEFAULT_USERNAME));
+        configClientProperties.setPassword(environment.getProperty(CloudConfigConstants.CONFIG_SERVER_PASSWORD, CloudConfigConstants.CONFIG_SERVER_DEFAULT_PASSWORD));
+        configClientProperties.setProfile(environment.getProperty(CloudConfigConstants.CONFIG_SERVER_PROFILE, CloudConfigConstants.CONFIG_SERVER_DEFAULT_PROFILE));
+        configClientProperties.setFailFast(!CloudConfigConstants.CONFIG_SERVER_DEFAULT_FAIL_FAST.equalsIgnoreCase(environment.getProperty(CloudConfigConstants.CONFIG_SERVER_FAIL_FAST, CloudConfigConstants.CONFIG_SERVER_DEFAULT_FAIL_FAST)));
+        configClientProperties.setFailFast(!CloudConfigConstants.CONFIG_SERVER_DEFAULT_ENABLE.equalsIgnoreCase(environment.getProperty(CloudConfigConstants.CONFIG_SERVER_ENABLE, CloudConfigConstants.CONFIG_SERVER_DEFAULT_ENABLE)));
 
         ConfigServicePropertySourceLocator configServicePropertySourceLocator = new ConfigServicePropertySourceLocator(configClientProperties);
         return configServicePropertySourceLocator.locate(environment);
     }
 
- }
+}
